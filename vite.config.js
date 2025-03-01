87480096884c56d9d33e8ba3591c4b4a7c661469
Sanjesh <sanjeshrg9@gmail.com>
2025-03-01 20:19:07 +0530

    Made PWA Setup 2

--- vite.config.js
+++ vite.config.js
@@ -16,18 +16,25 @@ export default defineConfig({
         short_name: "BubbleShare",
         description: "Instant Joy Sharing with Friends",
         theme_color: "#5ce1e6",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        scope: "/",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
         ],
+      },
+      devOptions: {
+        enabled: true,
       },
     }),
   ],
