16dd934a3746d7fdef12c1fcd07d75013a2e8272
Sanjesh <sanjeshrg9@gmail.com>
2025-03-01 15:24:15 +0530

    Added navbar in shared page

--- src/pages/SharedBubblesPage.jsx
+++ src/pages/SharedBubblesPage.jsx
@@ -37,42 +37,44 @@ const SharedBubblesPage = () => {
   }, []);
 
   return (
-    <div
-      className="h-screen w-full bg-cover bg-center fixed top-0 left-0 overflow-scroll p-4 pb-24"
-      style={{ backgroundImage: `url(${Bg})` }}
-    >
-      <div className="flex flex-col gap-6">
-        {sharedBubbles.map((bubble, index) => (
-          <motion.div
-            key={bubble.folderId}
-            className={`flex ${
-              index % 2 === 0 ? "justify-start" : "justify-end"
-            }`}
-            initial={{ y: 0 }}
-            animate={{ y: [0, -5, 5, 0] }}
-            transition={{
-              duration: 4,
-              repeat: Infinity,
-              ease: "easeInOut",
-              delay: index * 0.3,
-            }}
-          >
-            <Bubbles
-              bubble={bubble}
-              onClick={() => setSelectedBubbleId(bubble.folderId)}
-              onDelete={null}
-            />
-          </motion.div>
-        ))}
-      </div>
+    <div className="min-h-screen font-primary">
+      <div
+        className="h-screen w-full bg-cover bg-center fixed top-0 left-0 overflow-scroll p-4 pb-24"
+        style={{ backgroundImage: `url(${Bg})` }}
+      >
+        <div className="flex flex-col gap-6">
+          {sharedBubbles.map((bubble, index) => (
+            <motion.div
+              key={bubble.folderId}
+              className={`flex ${
+                index % 2 === 0 ? "justify-start" : "justify-end"
+              }`}
+              initial={{ y: 0 }}
+              animate={{ y: [0, -5, 5, 0] }}
+              transition={{
+                duration: 4,
+                repeat: Infinity,
+                ease: "easeInOut",
+                delay: index * 0.3,
+              }}
+            >
+              <Bubbles
+                bubble={bubble}
+                onClick={() => setSelectedBubbleId(bubble.folderId)}
+                onDelete={null}
+              />
+            </motion.div>
+          ))}
+        </div>
 
-      {selectedBubbleId && (
-        <BubbleDetails
-          bubbleId={selectedBubbleId}
-          onClose={() => setSelectedBubbleId(null)}
-          shared={true}
-        />
-      )}
+        {selectedBubbleId && (
+          <BubbleDetails
+            bubbleId={selectedBubbleId}
+            onClose={() => setSelectedBubbleId(null)}
+            shared={true}
+          />
+        )}
+      </div>
       <Navigation />
     </div>
   );
